c0849c1de26c1b62e7830e381481b0f7116581d4
asseternity <nakupovasset@gmail.com>
2025-08-03 14:43:07 +0400

    feat: incorporate dialogue UI

---
+++ DialogueUI.cs
@@ -0,0 +1,42 @@
+using System;
+using Godot;
+
+public partial class DialogueUI : Control
+{
+	private Label _textLabel;
+	private VBoxContainer _optionsContainer;
+	private Engine _engine;
+
+	public override void _Ready()
+	{
+		_engine = GetTree().Root.GetNode<Engine>("GlobalEngine");
+		_textLabel = GetNode<Label>("PanelContainer/VBoxContainer/TextLabel");
+		_optionsContainer = GetNode<VBoxContainer>("PanelContainer/VBoxContainer/OptionsContainer");
+	}
+
+	public void ShowStory(StoryLine story)
+	{
+		_textLabel.Text = story.Text;
+		foreach (var child in _optionsContainer.GetChildren())
+			child.QueueFree();
+
+		foreach (var option in story.Options)
+		{
+			var button = new Button();
+			button.Text = option.Text;
+			button.Pressed += () =>
+			{
+				_engine.ChooseOption(option.NextElement);
+				if (option.NextElement is StoryLine nextElementStory)
+					ShowStory(nextElementStory);
+				else if (option.NextElement is Duel nextElementDuel)
+					Hide();
+				else
+					Hide();
+			};
+			_optionsContainer.AddChild(button);
+		}
+
+		Show();
+	}
+}
--- Player.cs
+++ Player.cs
@@ -1,4 +1,20 @@
 using System;
 using Godot;
 
-public partial class Player : CharacterBody2D { }
+public partial class Player : CharacterBody2D
+{
+	[Export]
+	public float MovementSpeed = 100f;
+
+	public void GetInput()
+	{
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * MovementSpeed;
+	}
+
+	public void _physics_process(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
+}

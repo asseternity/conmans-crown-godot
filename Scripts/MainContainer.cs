8bb241bb6385626eff9141a1ba525a3292d18733
asseternity <nakupovasset@gmail.com>
2025-08-14 23:43:18 +0400

    feat: apartment level collision, intro dialogue, dialogic var retrieval

--- Scripts/MainContainer.cs
+++ Scripts/MainContainer.cs
@@ -4,6 +4,7 @@ using Godot.Collections;
 
 public partial class MainContainer : Node
 {
+	public string IntroTimelinePath = "res://dialogic/Timelines/intro.dtl";
 	private Engine _engine;
 	private Node? _dialogic;
 
@@ -36,6 +37,10 @@ public partial class MainContainer : Node
 			if (npc is NPC npcScript)
 				npcScript.InitNPC();
 		}
+
+		// Start intro dialogue
+		var dialogic = GetTree().Root.GetNodeOrNull("Dialogic");
+		dialogic.Call("start", IntroTimelinePath);
 	}
 
 	// Handler for Dialogic.signal_event
@@ -70,5 +75,35 @@ public partial class MainContainer : Node
 	}
 
 	// Called when any Dialogic timeline finishes (timeline_ended)
-	private void OnTimelineEnded() { }
+	private void OnTimelineEnded()
+	{
+		// If your variable lives in a folder, use "FolderName.SomeVariable".
+		var value = GetDialogicString("Dialogues.playerGender").ToLowerInvariant();
+		GD.Print(value);
+	}
+
+	private string GetDialogicString(string path)
+	{
+		if (_dialogic == null)
+			return "";
+
+		// Dialogic exposes its variable subsystem as the "VAR" property.
+		var varStoreV = _dialogic.Get("VAR");
+		if (varStoreV.VariantType != Variant.Type.Object)
+			return "";
+
+		var varStore = varStoreV.AsGodotObject();
+		if (varStore is null)
+			return "";
+
+		// subsystem_Variables has get_variable(name, default, no_warning)
+		Variant v = varStore.Call("get_variable", path, "");
+
+		return v.VariantType switch
+		{
+			Variant.Type.String => v.AsString(),
+			Variant.Type.StringName => v.AsStringName().ToString(),
+			_ => v.ToString()
+		};
+	}
 }
